4ade2c91eac81e37ba5c612d3d1597e63e380a80
James Waples <james@wapl.es>
2023-12-31 21:18:59 +0000

    Add final binary size to size-over-time script

--- embassy-size.sh
+++ embassy-size.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e "commit,date,text,bss,dec" > sizes.csv
+echo -e "commit,date,text,bss,dec,bin" > sizes.csv
 
 set -xe
 
@@ -18,8 +18,12 @@ do
         bss=$(echo $out | awk '{print $3}')
         dec=$(echo $out | awk '{print $4}')
 
+        cargo objcopy --release -- -O binary target/size.bin
+        out=$(wc -c target/demo.bin)
+        bin=$(echo $out | awk '{print $1}')
+
         popd
 
-        echo -e "$commit,$date,$text,$bss,$dec" >> sizes.csv
+        echo -e "$commit,$date,$text,$bss,$dec,$bin" >> sizes.csv
     fi
 done
