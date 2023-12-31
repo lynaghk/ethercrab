b31d6b3baf38ea1b308e9baf37df7aa80f5109ac
James Waples <james@wapl.es>
2023-12-31 21:20:08 +0000

    Ssshhhh

--- embassy-size.sh
+++ embassy-size.sh
@@ -2,7 +2,7 @@
 
 echo -e "commit,date,text,bss,dec,bin" > sizes.csv
 
-set -xe
+set -e
 
 for commit in $(git rev-list master)
 do
@@ -11,14 +11,16 @@ do
 
         date=$(git show -s --format=%ci $commit)
 
-        git checkout $commit
+        echo "Commit ${commit} at ${date}"
 
-        out=$(cargo size --release | tail -n1)
+        git checkout $commit --quiet
+
+        out=$(cargo size --release --quiet | tail -n1)
         text=$(echo $out | awk '{print $1}')
         bss=$(echo $out | awk '{print $3}')
         dec=$(echo $out | awk '{print $4}')
 
-        cargo objcopy --release -- -O binary target/size.bin
+        cargo objcopy --release --quiet -- -O binary target/size.bin
         out=$(wc -c target/demo.bin)
         bin=$(echo $out | awk '{print $1}')
 
@@ -27,3 +29,5 @@ do
         echo -e "$commit,$date,$text,$bss,$dec,$bin" >> sizes.csv
     fi
 done
+
+echo "Done"
